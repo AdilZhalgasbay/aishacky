7fe00e915aedca76faec66017b9193dd3958c11e
Adil Zhalgasbay <zhalgasbayadil@gmail.com>
2026-04-19 09:29:26 +0500

    gggg

--- start_all.bat
+++ start_all.bat
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k "cd web && npm run dev -- --hostname 0.0.0.0 --port 3000"
+start cmd /k "cd web && npm run build && npm start -- --hostname 0.0.0.0 --port 3000"
 start cmd /k ".venv\Scripts\activate && uvicorn app.main:app --host 0.0.0.0 --port 8000"
 start cmd /k "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:8000 serveo.net"
